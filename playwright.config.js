1bdaeea172084590e0f2bff5dd8af07060e01b96
Lucas Porto <lucas-porto1@hotmail.com>
2026-08-08 22:53:02 -0300

    refactor: improve Playwright POM test architecture

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 export default defineConfig({
   testDir: './tests',
